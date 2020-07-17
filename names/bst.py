27b494cde204afb26461b2e9d448267b60c200a8
Jessica Williams <32785700+JayellWolfe@users.noreply.github.com>
2020-07-17 12:22:11 -0400

    Create bst.py
    added bst.py

---
+++ names/bst.py
@@ -0,0 +1,35 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):       
+        if self.value == target:
+            return True    
+        else:           
+            if target < self.value:               
+                if not self.left:
+                    if self.left.value == target:
+                        return True
+                else:
+                    self.left.contains(target)
+            else:                
+                if not self.right:
+                    return False                
+                if self.right.value == target:
+                    return True
+                else:
+                    self.right.contains(target)
